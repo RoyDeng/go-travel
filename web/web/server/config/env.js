8d8d0f668556224707815b5c6f9610f0dd5440c1
Yu-Teng Deng <xoxo4795946@yahoo.com.tw>
2019-07-05 17:01:57 +0800

    nginx proxy to api

--- web/web/server/config/env.js
+++ web/web/server/config/env.js
@@ -5,4 +5,4 @@ dotenv.config({path: process.env.PATH_ENV});
 export const JWT_SECRET = process.env.SECRET || 'secret';
 export const PORT = process.env.PORT || 5000;
 export const NODE_ENV = process.env.NODE_ENV;
-export const DATABASE = process.env.DATABASE_URL || '';
+export const DATABASE = process.env.DATABASE_URL || 'db';
